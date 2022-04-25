5befb37bf00e244a458406b298ef43e2b42c5a99
Nilay <54245048+dnilay@users.noreply.github.com>
2022-04-25 11:33:09 +0530

    Add files via upload

--- 2ndhandson/src/main/java/com/example/demo/Application.java
+++ 2ndhandson/src/main/java/com/example/demo/Application.java
@@ -1,23 +1,34 @@
 package com.example.demo;
 
-import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		displayDate(new Date());
+	}
+	
+	public static void displayDate(Date date)
+	{
+		log.info("START");
 		org.springframework.context.ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-		SimpleDateFormat dateFormat=context.getBean("dateFormat",SimpleDateFormat.class);
-		java.util.Date date=new java.util.Date();
+		SimpleDateFormat dateFormat = context.getBean("dateFormat", SimpleDateFormat.class);
 		
-		String strDate= dateFormat.format(date);  
+
+		String strDate = dateFormat.format(date);
+		log.debug(strDate);
 		System.out.println(strDate);
+		log.info("END");
 	}
 
 }
